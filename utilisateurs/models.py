87eccb2ddbb0faf2371a883a5fe5e65ba6c3201a
michasacha <guiademkouammichelesacha@gmail.com>
2024-06-27 20:27:40 +0800

    contenu de la classe utilisateur

--- utilisateurs/models.py
+++ utilisateurs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import AbstractUser from django.db import models
 
+class Utilisteur(AbstractUser):
+    date_of_birth= models.DateField(null=True, blank=True)
+        phone_number= models.CharField(max_length=15, blank=True)
+            matricule= models.CharField(max_length=15, blank=False)
+                profile_photo= models.ImageField(upload_to='profil_photos/', null=True, blank=True)
+                sex= models.CharField(max_length=1, blank=False)
 # Create your models here.
