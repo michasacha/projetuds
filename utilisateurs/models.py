d552f4366b6c47c285edad679c5f7d0b510e65f6
FranckNgwewe <ngwewefranck@gmail.com>
2024-06-29 10:38:26 +0100

    test ok

--- utilisateurs/models.py
+++ utilisateurs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import AbstractUser from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Utilisteur(AbstractUser):
     date_of_birth= models.DateField(null=True, blank=True)
