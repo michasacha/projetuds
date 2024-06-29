e33f1f431616e353fdf1deacec3a96e34738fbd3
Michmira <mirabelleteubou@gmail.com>
2024-06-29 17:10:55 +0100

    t

--- projetuds/settings.py
+++ projetuds/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'django.midd leware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
